ca70a4a3af92b2a40b363b745c2a689434ea9eca
nbmcguire <57973784+nbmcguire@users.noreply.github.com>
2025-02-12 16:33:14 -0500

    Initial Commit

---
+++ BipTheGuy/BipTheGuyApp.swift
@@ -0,0 +1,17 @@
+//
+//  BipTheGuyApp.swift
+//  BipTheGuy
+//
+//  Created by Noah McGuire on 2/12/25.
+//
+
+import SwiftUI
+
+@main
+struct BipTheGuyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
